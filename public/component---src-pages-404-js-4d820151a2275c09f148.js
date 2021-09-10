6731e5264b148950bc339d84fff9d0abb9668833
erp7 <erp7@akr.com>
2021-09-10 18:15:18 +0530

    deploy

--- public/component---src-pages-404-js-4d820151a2275c09f148.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunkgatsby_mui_starter=self.webpackChunkgatsby_mui_starter||[]).push([[883],{9616:function(t,e,u){"use strict";u.r(e),u.d(e,{default:function(){return r}});var n=u(7294);function r(){return n.createElement("div",null,"404 Not Found")}}}]);
-//# sourceMappingURL=component---src-pages-404-js-4d820151a2275c09f148.js.map
